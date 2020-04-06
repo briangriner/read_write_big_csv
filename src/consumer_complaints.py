9198e8f102e4bd0121da354cf6f578f027ab1f09
briangriner <briangriner@protonmail.com>
2020-04-06 15:37:06 -0400

    update py reader

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -2,26 +2,77 @@
 # consumer_complaints.py - reads input/complaints.csv file from input dir and creates a report written to output/report.csv
 
 import csv
+from collections import Counter
 
 # data file paths
-indata = 'input/complaints_sm.csv'
-outdata = 'output/report.csv'
+indata = '../input/complaints_11.csv'
+outdata = '../output/report.csv'
 
-# input datafile very large so use csv module with generator to read input data one line at a time
+class ReadBigCSV(object):
+    def __init__(self, path):
+        self.path  = path
+        self._length = None
+        self._counter = None
 
-def read_big_csv(file_path):
-    """Designed to read large csv files efficiently with low overhead using csv module with generator.
-    Assumes 1st row of csv contain field names."""
+    
+    def __iter__(self):
+        self._length = 0
+        self._counter_p = Counter()
+        self._counter_d = Counter()
+        with open(self.path, 'r') as data:
+            reader = csv.DictReader(data)
+            for row in reader:
+                # save stats
+                self._length += 1
+                self._counter_p[row['Product']] += 1
+                self._counter_d[row['Date received']] += 1
+                yield row
 
-    with open(file_path, 'rU') as data:
-        reader = csv.DictReader(data)
-        for row in reader:
-            yield row
+
+    def __len__(self):
+        if self._length is None:
+            for row in self:
+                continue
+            return self._length
 
 
-    if __name__ == "__main__":
-        for idx, row in enumerate(read_big_csv(indata)):
-            if idx > 10: break
-            print("Number of rows read_big_csv() read in: ", rows)
-            
+    @property
+    def counter_p(self):
+        if self._counter_p is None:
+            for row in self:
+                continue
+        return self._counter_p
+
+
+    @property
+    def products(self):
+        return self.counter_p.keys()
+
+
+    @property
+    def counter_d(self):
+        if self._counter_d is None:
+            for row in self:
+                continue
+        return self._counter_d
+
+
+    @property
+    def dates(self):
+        return self.counter_d.keys()
+
+
+    def reset(self):
+        """Used if break during read"""
+        self._length = None
+        self._counter_p = None
+        self._counter_d = None
+
+
+if __name__ == "__main__":
+    reader = ReadBigCSV(indata)
+    print("%i total complaints with  %i products and %i dates" % (len(reader), len(reader.products), len(reader.dates)))
+    for i, row in enumerate(reader):
+        print(i, row['Product'], row['Date received'])
+
 
