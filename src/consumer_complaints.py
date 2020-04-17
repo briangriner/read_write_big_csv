349ce3b30a27a5ca01942fc3341ea5392332cb6f
Brian Griner, PhD <briangriner@protonmail.com>
2020-04-17 00:05:56 -0400

    updated source with pathlib module

--- src/consumer_complaints.py
+++ src/consumer_complaints.py
@@ -1,14 +1,20 @@
-#! /user/bin/python3
+#! /usr/bin/python3
 """consumer_complaints.py - reads large csv, maps fields to named tuples, uses generator with reduce to compute total, 
 uses reducer func with default dict to create list of dicts""" 
 
-import csv, itertools
+import csv, itertools, os
+from pathlib import Path
 from collections import Counter, namedtuple, defaultdict
 from functools import reduce
 
 # data file paths
-indata = '../input/complaints_11.csv'
-outdata = '../output/report.csv'
+indata =  'insight_consumer_complaint_reporting/input/complaints_11.csv'
+outdata = 'insight_consumer_complaint_reporting/output/report.csv'
+
+# pathlib .joinpath()
+home_path = Path.home()
+input_path = home_path.joinpath(indata)
+output_path = home_path.joinpath(outdata)
 
 # namedtuple instead of dict
 fields = ("Date_received", "Product", "Sub_product", "Issue", "Sub_issue", "Consumer_complaint_narrative", "Company_public_response", "Company", "State", "ZIP_code", "Tags", "Consumer_consent_provided", "Submitted_via", "Date_sent_to_company", "Company_response_to_consumer", "Timely_response", "Consumer_disputed", "Complaint_ID")
@@ -38,14 +44,14 @@ def reducer(acc, val):
 
 
 # use reduce to calculate totals and subtotals
-total_complaints = reduce(lambda acc, val: acc + 1, read_big_csv(indata), 0)
-prod_date = reduce(reducer, read_big_csv(indata), defaultdict(list))
+total_complaints = reduce(lambda acc, val: acc + 1, read_big_csv(input_path), 0)
+prod_date = reduce(reducer, read_big_csv(input_path), defaultdict(list))
 print(type(prod_date))
 
 
 if __name__ == "__main__":
     print("Program starting")
-    for row in read_big_csv(indata):
+    for row in read_big_csv(input_path):
         prod_date
 
 
@@ -53,7 +59,7 @@ print("prod_date")
 print("-"*72)
 print(prod_date)
 
-wdata = open(outdata, "a", newline="")
+wdata = open(output_path, "a", newline="")
 writer = csv.writer(wdata, delimiter=",", quoting=csv.QUOTE_NONNUMERIC)
 names = ["product", "year", "total number of complaints", "total number of companies"] 
 writer.writerow(names)
