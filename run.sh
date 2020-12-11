82e1328972e6099c0f42bb59d4578fa767149698
Brian Griner, PhD <34611149+briangriner@users.noreply.github.com>
2020-12-10 23:10:28 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # make.sh - run consumer_complaints.py to read ../input/complaints.csv and create ../output/report.csv
 
-python3 -u ~/insight_consumer_complaint_reporting/src/consumer_complaints.py > consumer_complaints.py.log 
+python3 -u ~/read_write_big_csv/src/consumer_complaints.py > consumer_complaints.py.log 
